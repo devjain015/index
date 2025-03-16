75827c51a5af4190326c0ad4db21a2a0adb9b2e8
devjain015 <dmg9265615930@gmail.com>
2025-03-16 20:17:00 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,8 +7,8 @@ const categories = [
   },
   {
     id: 8,
-    name: "Plastic Cup",
-    image: "https://5.imimg.com/data5/JB/KW/YD/SELLER-6260598/plastic-coke-glass-500x500.jpg",
+    name: "Plastic Coffee Mug",
+    image: "https://5.imimg.com/data5/JB/KW/YD/SELLER-6260598/plastic-coke-glass-500x500.jpg","https://sigmawire.net/i/03/cnTVf6.jpg",
     description: "Our range of products include plastic cup with leed, plastic cup with leed., plastic tea cup, plastic cup and plastic cup with star design.",
   },
   {
